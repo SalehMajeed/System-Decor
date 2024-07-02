0e6c19c467b09a50bd6a8dbec185572477488ecc
Asif Khan <ak3226519@gmail.com>
2024-07-02 16:17:35 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -3,15 +3,7 @@ const callBtnAfter = window.getComputedStyle(callBtn, "::after")
 const header = document.getElementById("header")
 const mobileMenuButton = document.getElementById("mobileMenuButton")
 
-// console.log(callBtnAfter.borderRadius)
-callBtn.addEventListener("hover", () => {
 
-})
-
-const menu = document.getElementById("menu")
-const close = document.getElementById("close")
-
-console.log(menu)
 menu.addEventListener('click', () => {
     menu.style.transform = "translate(0,-100%)"
     close.style.transform = "translate(0,-100%)"
@@ -26,26 +18,3 @@ close.addEventListener('click', () => {
     header.style.padding = "5px 0"
 })
 
-// const add = (a) => (b) => (c) => (d) => () => a + b + c + d
-
-// console.log(add(5)(2)(4)(5)())
-
-
-// const calc = {
-//     total: 0,
-//     add(a) {
-//         this.total += a
-//         return this
-//     },
-//     multiply(a) {
-//         this.total *= a
-//         return this
-//     },
-//     subtract(a) {
-//         this.total -= a
-//         return this
-//     },
-// }
-
-// const result = calc.add(10).multiply(5).subtract(30).add(10)
-// console.log(result.total)
