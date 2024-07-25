f85c688b27834d355d70c0a29dd7fa0eaa0932f0
Asif <ak3226519@gmail>
2024-07-26 01:16:44 +0530

    image animation and text animation fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
+
+// script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const letters = document.querySelectorAll('.letter');
-    letters.forEach((letter, index) => {
-        setTimeout(() => {
-            letter.classList.add('show');
-        }, index * 100);
+    const textContents = [
+        "ARCHITECTURAL",
+        "ACOUSTIC",
+        "SOLUTIONS"
+    ];
+
+    textContents.forEach((text, index) => {
+        const container = document.getElementById(`text${index + 1}`);
+        const letters = text.split('');
+        const middleIndex = Math.floor(letters.length /2);
+
+        letters.forEach((letter, letterIndex) => {
+            const span = document.createElement('span');
+            span.classList.add('letter');
+            span.innerText = letter;
+            const distanceFromMiddle = Math.abs((letterIndex - middleIndex)/2);
+            span.style.transitionDelay = `${distanceFromMiddle * 0.3}s`;
+            container.appendChild(span);
+        });
     });
-})
+
+    setTimeout(() => {
+        const letters = document.querySelectorAll('.letter');
+        letters.forEach((letter) => {
+            letter.classList.add('animate');
+        });
+    }, 100); // Start animation after a brief delay
+});
